cdeb4f8d88d592609b2012def7f11dd64107bac8
danvisintainer <dan@danvisintainer.com>
2015-05-06 00:37:55 -0400

    make class and board initializer

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,3 +1,28 @@
-class tictactoe_game
+require 'pry'
+
+class TictactoeGame
+
+  def initialize
+    @board = []
+  end
+
+  def initialize_board(n)
+    n.times do |row|
+      @board[row] = []
+      n.times do |column|
+        @board[row][column] = '-'
+      end
+    end
+
+    puts @board.inspect
+  end
+
+  def go
+    puts "Let's play Tic-Tac-Toe!"
+    puts "How big do you want your board?"
+    initialize_board(gets.chomp.to_i)
+  end
 
 end
+
+TictactoeGame.new.go
